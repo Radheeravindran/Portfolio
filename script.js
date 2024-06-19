bbc5c86bb13d1a3cfa880329e9599e9162734526
Radheeravindran <166691905+Radheeravindran@users.noreply.github.com>
2024-06-19 20:51:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+Let type = new Typed('#changing-text'),{
+    strings : ['Junior FrontEnd Developer','Junior FrontEnd Developer','Junior FrontEnd Developer'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+}
